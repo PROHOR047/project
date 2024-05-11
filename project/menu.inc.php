2ed405948e85c7f8a4055796d6dab166c16047ce
PROHOR047 <ivanglazkov317@gmail.com>
2024-05-11 15:57:50 +0300

    Add files via upload

---
+++ project/menu.inc.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="https://youtu.be/dQw4w9WgXcQ?si=z3_2yiCLB1BMEyvF"> About my studies </a></li>
+                    <li><a href="https://steamcommunity.com/profiles/76561198815178773/"> My portfolio </a></li>
+                    <li><a href="https://yandex.ru/images/search?pos=1&from=tabbar&img_url=https%3A%2F%2Fcdn4.telegram-cdn.org%2Ffile%2FTZXJtzw413hysIBWMWqpqzIlR_6nY_7omZNyZRn3G4EUGkUzyQi9k1oKGnLs3NoQm329sapOxkpxoONfIk11OZ6yvKUGa5nXK_oawi_ONXnBR-ze0aWBKYhQHR0XADe0b5hAzr-R7zxeeS2phlWci-Vk_pKOANyQeatfHVW72XsERD7ojewqYMPKeb8a_ud5U0jwmYaC-8wkTfcyY2AGdMEJoVJyUJ8hEKbnsnm_45g8xhWtiJetHMGavkvC8WtA6aAy3HW0A0VL7RsjGgv5Fx2hsq8ZfSu5AmXjPVI-UGez_5rfe1CTymt7Hp4XvcNJUQWXfE2S-z86p1vPfIJFQg.jpg&text=я+не+придумал+что+сюда+вставить+картинка&rpt=simage&lr=20610"> My articles </a></li>
+                    <li><a href="http://www.youtube.com/@Van_WasG"> Links </a></li>
+                    <li><a href="https://vk.com/van_was"> Contacts </a></li>
+                </ul>    
+            </nav>
